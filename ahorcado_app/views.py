378db8d7c7ead689b868ecf6eee59bfffd1f1bf1
Federico Acosta <facosta@eventbrite.com>
2020-04-18 17:34:09 -0300

    highscore html

--- ahorcado_app/views.py
+++ ahorcado_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Highscore
+
+
+class HighscoreView(ListView):
+    model = Highscore
+
 
 # Create your views here.
