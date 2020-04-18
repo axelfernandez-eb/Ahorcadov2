efc8c61d7385a7e47e7bd89716e05b6b18b76840
stefania arias <sarias@eventbrite.com>
2020-04-18 17:06:41 -0300

    aadding urls patterns for login

--- Ahorcadov2/urls.py
+++ Ahorcadov2/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from ahorcado_app import urls as ahorcado_app_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(ahorcado_app_urls)),
 ]
