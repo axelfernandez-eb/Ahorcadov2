9a4c088f6e12f244f7589dfe452fc82dba6dd123
Federico Acosta <facosta@eventbrite.com>
2020-04-19 12:31:12 -0300

    agregado boton para crear cuenta

--- ahorcado_app/urls.py
+++ ahorcado_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls'), name="login"),
     path('highscore/', HighscoreView.as_view(), name='highscores'),
     path('game/', Game.as_view(), name='game'),
-    path('register/',register, name='register')
+    path('register/',register, name='register'),
 ]
