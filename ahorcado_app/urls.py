464faf288b9f9c938411bf8375ddb6e43d6ad582
Federico Acosta <facosta@eventbrite.com>
2020-04-19 12:09:22 -0300

    htmls finished

--- ahorcado_app/urls.py
+++ ahorcado_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from .models import Game
-from .views import HighscoreView
+from .views import HighscoreView, register
 
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls'), name="login"),
     path('highscore/', HighscoreView.as_view(), name='highscores'),
-    path('game', Game.as_view(), name='game')
+    path('game/', Game.as_view(), name='game'),
+    path('register/',register, name='register')
 ]
--- ahorcado_app/views.py
+++ ahorcado_app/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
-from .models import Highscore, Game
-from .ahorcado import Ahorcado
+from .models import Highscore
+from django.contrib.auth.forms import UserCreationForm
 
 
 class HighscoreView(ListView):
     model = Highscore
+
+
+def register(request):
+    if request.method == "POST":
+
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'ahorcado_app/create_user.html', {'form': form})
